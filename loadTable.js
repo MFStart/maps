dc89dcc0275dd1de9b0647540892a879c4421df3
MFStart <MFStart@github.com>
2020-08-12 00:38:04 -0700

    update table as an object. made classification.

--- loadTable.js
+++ loadTable.js
@@ -1,35 +1,37 @@
-d3.text("covidbyzip.csv").then(function (data){
+var table = d3.text('covidbyzip.csv').then(function (data){
     var rows  = d3.csvParseRows(data);
     table = d3.select('body').append('table')
-    .style("border-collapse", "collapse")
-    .style("border", "2px black solid");
+    .style('border-collapse', 'collapse')
+    .style('border', '2px black solid');
     // headers
-    table.append("thead").append("tr")
-        .selectAll("th")
+    table.append('thead').append('tr')
+        .selectAll('th')
         .data(rows[0])
-        .enter().append("th")
+        .enter().append('th')
         .text(function(d) { return d; })
-        .style("border", "1px black solid")
-        .style("padding", "5px")
-        .style("background-color", 'steelblue')
-        .style("font-weight", "bold")
-        .style("text-transform", "uppercase");
+        .style('border', '1px black solid')
+        .style('padding', '5px')
+        .style('background-color', 'steelblue')
+        .style('font-weight', 'bold')
+        .style('text-transform', 'uppercase');
     //body
-    table.append("tbody")
-        .selectAll("tr").data(rows.slice(1))
-        .enter().append("tr")
+    table.append('tbody')
+        .selectAll('tr').data(rows.slice(1))
+        .enter().append('tr')
+        //hover change color
         .on('mouseover',function(d) {
-            d3.select(this).style("background-color","orange")})
-        .on("mouseout",function(d){
-          d3.select(this)
-          .style("background-color","transparent");
-        })
-        .selectAll("td")
+            d3.select(this).style('background-color','orange')})
+        .on('mouseout',function(d){
+          d3.select(this).style('background-color','transparent');})
+        .selectAll('td')
         .data(function(d){return d;})
-        .enter().append("td")
-        .style("border", "1px black solid")
-        .style("padding", "5px")
+        .enter().append('td')
+        .on('click',function(d){
+          returm
+        })
+        .style('border', '1px black solid')
+        .style('padding', '5px')
         .text(function(d){return d;})
-        .style("font-size", "12px")
+        .style('font-size', '12px')
 
       });
--- map.js
+++ map.js
@@ -2,20 +2,58 @@ var map = L.map('map', {
   center: [37.35105530964274, -121.95716857910155], // EDIT latitude, longitude to re-center map
   zoom: 12,  // EDIT from 1 to 18 -- decrease to zoom out, increase to zoom in
   scrollWheelZoom: true });
-var zipcode = L.esri.featureLayer({ url: 'https://services.arcgis.com/NkcnS0qk4w2wasOJ/ArcGIS/rest/services/COVIDCasesByZipCode/FeatureServer/0' });
+
+var zipcode = L.esri.featureLayer({
+  url: 'https://services.arcgis.com/NkcnS0qk4w2wasOJ/ArcGIS/rest/services/COVIDCasesByZipCode/FeatureServer/0',
+  simplifyFactor: .5,
+  precision: 5,
+  style: function (feature){
+          var x = feature.properties.Cases;
+          if (x > 1000) return {color:'#b10026',};
+          if (x > 800)  return {color:'#e31a1c'};
+          if (x > 500)  return {color:'#fc4e2a'};
+          if (x > 300) return {color:'#fd8d3c'};
+          if (x > 100)  return {color:'#feb24c'};
+          if (x > 50)   return {color:'#fed976'};
+          if (x > 10)   return {color:'#ffffb2'};
+          else return {color:'#ffffcc'};}
+}).addTo(map);
+
 
 /* display basemap tiles -- see others at https://leaflet-extras.github.io/leaflet-providers/preview/ */
-L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', { attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
-}).addTo(map);
-var popupTemplate = '<h3>{zipcode}</h3><br>Population: {Population}</br>';
+var popupTemplate = '<h1>{zipcode}</h3> <br>Cases: {Cases}</br><br>Population: {Population}</br>';
 /* Display a point marker with pop-up text */
 //L.marker([37.35105530964274, -121.95716857910155]).addTo(map).bindPopup('text here'); // EDIT pop-up text messag
 zipcode.bindPopup(function (x) {
   return L.Util.template(popupTemplate, x.feature.properties);
 });
 
-/* Control panel to display map layers */
+/*
 
+zipcode.on('mouseover', function (e) {
+  document.getElementById('info-pane').innerHTML = e.layer.feature.properties.zipcode;
+  var layer = e.target;
+  layer.setStyle({
+    color: '#9D78D2',
+    weight: 5,
+    opacity: 1
+  });
+});
+*/
+
+/*function query_zipcode(x) {
+
+    zipcode.query().where(x.).run(function (error, featureCollection, response) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      console.log(featureCollection.features[0].properties.zipcode);
+    })
+
+
+}
+*/
 
 /* Carto light-gray basemap tiles with labels */
 var light = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
@@ -34,11 +72,9 @@ var based= {
 "Terrain": terrain
 //define feature layers
 };
+
 var layers={
 "zipcode": zipcode
 };
 //controller for layers
-var controlLayers = L.control.layers( based,layers, {
-position: "topright",
-collapsed: false
-}).addTo(map);
+var controlLayers = L.control.layers(based,layers,{collapsed:false}).addTo(map);
