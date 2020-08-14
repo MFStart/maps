4f9cd1012e7a267233529e6da7bec38a008a3772
MFStart <MFStart@github.com>
2020-08-14 01:00:39 -0700

    table color in progress

--- loadTable.js
+++ loadTable.js
@@ -1,4 +1,9 @@
 var table = d3.text('covidbyzip.csv').then(function (data){
+var keep_click = false;
+var local = d3.local();
+/*document.querySelectorAll(map)
+  .forEach(e => e.addEventListener("click", clickHandler));
+*/
     var rows  = d3.csvParseRows(data);
     table = d3.select('body').append('table')
     .style('border-collapse', 'collapse')
@@ -18,17 +23,31 @@ var table = d3.text('covidbyzip.csv').then(function (data){
     table.append('tbody')
         .selectAll('tr').data(rows.slice(1))
         .enter().append('tr')
+        .on('click',function(d){
+          keep_click = !keep_click;
+          d3.select(this).style('background','lightblue');
+          })
         //hover change color
         .on('mouseover',function(d) {
-            d3.select(this).style('background-color','orange')})
+            if(d3.select(this).style('background') == 'lightblue')
+              d3.select(this).style('background','lightblue')
+            else
+              d3.select(this).style('background', 'orange');
+          })
         .on('mouseout',function(d){
-          d3.select(this).style('background-color','transparent');})
+          if(keep_click){
+            d3.select(this).style('background','lightblue');
+            keep_click = !keep_click;
+            }
+          else
+            d3.select(this).style('background','transparent');
+          })
         .selectAll('td')
         .data(function(d){return d;})
         .enter().append('td')
-        .on('click',function(d){
-          returm
-        })
+        /*.on('click',function(d){
+          if
+          alert('you clicked on : ' + d);})*/
         .style('border', '1px black solid')
         .style('padding', '5px')
         .text(function(d){return d;})
